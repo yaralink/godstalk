4ece1577604e55adc835e3d112b4bcc484d08400
Yara Link <yaralink@pm.me>
2021-09-22 22:25:15 +0545

    create github pages

---
+++ assets/mbr-tabs/mbr-tabs.js
@@ -0,0 +1,5 @@
+
+function outerFind(a,b){var d=Array.from(a.querySelectorAll(b));a.matches(b)&&d.splice(0,0,a);return d}
+function updateId(a){0!==a.querySelectorAll(".nav-tabs").length&&outerFind(a,'section[id^="tabs"], section.tabs, section[id^="extTabs"]').forEach(function(b){var d=b.getAttribute("id"),f=b.querySelectorAll(".nav-tabs > .nav-item");b.querySelectorAll(".tab-pane").forEach(function(a,b){0===b?a.classList.contains("active")||a.classList.add("active"):(a.classList.remove("active"),a.setAttribute("id",d+"_tab"+b))});f.forEach(function(b,e){var c=b.querySelector("a");"5"==a.getAttribute("data-bs-version")&&
+c.setAttribute("data-bs-toggle","tab");0===e?c.classList.contains("active")||c.classList.add("active"):(c.classList.remove("active"),c.removeAttribute("active"),c.setAttribute("href","#"+d+"_tab"+e))})})}var $,isJQuery="function"==typeof jQuery;isJQuery&&($=jQuery);var isBuilder=document.querySelector("html").classList.contains("is-builder");
+if(isBuilder&&isJQuery)$(document).on("add.cards",function(a){updateId(a.target)});else"undefined"===typeof window.initTabsPlugin&&(window.initTabsPlugin=!0,updateId(document.body));
